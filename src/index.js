69e1c725202ba9dcbcb6de645de00431ca6b7b46
nguyenhoangthienchi <nguyenhoangthienchi@gmail.com>
2022-04-18 19:06:10 +0700

    update text

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    Hello kq nua ne hehe
+    New text here
     <App />
   </React.StrictMode>
 );
