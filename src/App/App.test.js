dd9b22666672aa19314d2f0486c46809bd56ca2a
nguyenhoangthienchi <nguyenhoangthienchi@gmail.com>
2022-04-18 23:44:54 +0700

    update unit test for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -4,6 +4,6 @@ import { screen, render } from '@testing-library/react';
 describe('Should render App properly', () => {
     it ('render App', async () => {
         render(<App />);
-        expect(await screen.findByText('Hello X-Force new text')).toBeVisible();
+        expect(await screen.findByText('Hello X-Force new text hehe')).toBeVisible();
     });
 })
